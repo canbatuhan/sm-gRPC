cba5d127c15d9d720b0838cb8430a44a1d6a2206
ituitis-canb18 <canbatuhan.official@gmail.com>
2021-12-21 18:19:57 +0300

    TwoPhaseCommitServices methods are updated

--- smgrpc/src/main/java/com/cloudlab/TwoPhaseCommitServices.java
+++ smgrpc/src/main/java/com/cloudlab/TwoPhaseCommitServices.java
@@ -7,11 +7,12 @@ import com.cloudlab.grpc.Tpc.AllocationResponse;
 import com.cloudlab.grpc.Tpc.NotificationMessage;
 import com.cloudlab.grpc.Tpc.Empty;
 import com.cloudlab.grpc.tpcGrpc.tpcImplBase;
-import com.cloudlab.utils.Events;
 import com.cloudlab.utils.States;
 import io.grpc.stub.StreamObserver;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * TwoPhaseCommitServices
@@ -19,20 +20,33 @@ import java.util.HashMap;
  */
 public class TwoPhaseCommitServices extends tpcImplBase {
     private Integer timestamp;
-    private HashMap<String, States> clientMap;
-    private HashMap<States, Boolean> stateMap;
+    private HashMap<String, Boolean> variableMap;
+    private HashSet<String> clientMap;
 
+
+    /**
+     * Builds TwoPhaseCommitServices object
+     */
     public TwoPhaseCommitServices() {
         this.timestamp = 0;
-        this.clientMap = new HashMap<>();
-        this.stateMap = new HashMap<>();
+        this.variableMap = new HashMap<>();
+        this.clientMap = new HashSet<>();
     }
 
+    /**
+     * Updates its timestamp according to the incoming message
+     * @param timestamp incoming timestamp
+     */
     public void updateTimestamp(Integer timestamp) {
         if (this.timestamp < timestamp) this.timestamp = timestamp + 1;
         else this.timestamp = this.timestamp + 1;
     }
 
+    /**
+     * Generates connection response message for client
+     * @param response boolean value, server's on greeting the client
+     * @return ConnectionResponse message
+     */
     public ConnectionResponse generateConnectionResponse(boolean response) {
         return ConnectionResponse
                 .newBuilder()
@@ -41,6 +55,11 @@ public class TwoPhaseCommitServices extends tpcImplBase {
                 .build();
     }
 
+    /**
+     * Generates allocation response message to send to client
+     * @param response boolean value, server's answer on allocation request
+     * @return AllocationResponse message
+     */
     public AllocationResponse generateAllocationResponse(boolean response) {
         return AllocationResponse
                 .newBuilder()
@@ -49,6 +68,10 @@ public class TwoPhaseCommitServices extends tpcImplBase {
                 .build();
     }
 
+    /**
+     * Generates empty message (response of notifyingService)
+     * @return Empty message
+     */
     public Empty generateEmpty() {
         return Empty
                 .newBuilder()
@@ -68,19 +91,20 @@ public class TwoPhaseCommitServices extends tpcImplBase {
         /* response logic of greeting service */
         boolean response;
 
-        if (!clientMap.containsKey(clientID)) {
-            System.out.println("Client " + clientID + " has connected.");
-            this.clientMap.put(clientID, States.S_init);
-            this.stateMap.replace(States.S_init, true);
-            response = true;
-        }
-
-        else {
+        // if the client is already connected, ignore message
+        if (this.clientMap.contains(clientID)) {
             System.out.println("Client " + clientID + " is already connected.");
             response = false;
         }
 
-        /* generate and send the response */
+        // otherwise, say hello
+        else {
+            this.clientMap.add(clientID);
+            System.out.println("Hello " + clientID + ".");
+            response = true;
+        }
+
+        /* generating and sending the response */
         this.updateTimestamp(timestamp);
         ConnectionResponse connectionResponse = this.generateConnectionResponse(response);
         responseObserver.onNext(connectionResponse);
@@ -89,7 +113,7 @@ public class TwoPhaseCommitServices extends tpcImplBase {
 
     /**
      * allocationService, used when a client wants to allocate a process time from the server
-     * @param request, allocation message includes clientID, timeStamp and eventName
+     * @param request, allocation message includes clientID, timeStamp, readFrom and writeTo
      * @param responseObserver sender of the response
      */
     @Override
@@ -97,10 +121,35 @@ public class TwoPhaseCommitServices extends tpcImplBase {
         String clientID = request.getClientID();
         Integer timestamp = request.getTimestamp();
 
-        /* response logic (temporary) of allocation service */
-        boolean response = false;
+        /* response logic of allocation service */
+        boolean response = true; // initially true
+        String writeVariable;
 
-        /* generate and send the response */
+        int numOfWriteVariables = request.getWriteToCount();
+        for (int index=0; index<numOfWriteVariables; index++) {
+            writeVariable = request.getWriteTo(index);
+
+            // if the server face with this variable for the firs time, add it to map
+            if (!this.variableMap.containsKey(writeVariable)) {
+                this.variableMap.put(writeVariable, false);
+            }
+
+            // if the variable is "in-use", then server can not allocate for this event
+            else if (this.variableMap.get(writeVariable)){
+                response = false;
+                break;
+            }
+        }
+
+        // if the server is going to allocate for the event, set the variables as "in-use"
+        if (response) {
+            for (int index=0; index<numOfWriteVariables; index++) {
+                writeVariable = request.getWriteTo(index);
+                this.variableMap.replace(writeVariable, true);
+            }
+        }
+
+        /* generating and sending the response */
         this.updateTimestamp(timestamp);
         AllocationResponse allocationResponse = this.generateAllocationResponse(response);
         responseObserver.onNext(allocationResponse);
@@ -109,7 +158,7 @@ public class TwoPhaseCommitServices extends tpcImplBase {
 
     /**
      * notifyingService, used when a client done with its process and release the allocation.
-     * @param request, notifying message includes clientID, timeStamp and eventName
+     * @param request, notifying message includes clientID, timeStamp, readFrom and writeTo
      * @param responseObserver, sender of the response
      */
     @Override
@@ -117,10 +166,16 @@ public class TwoPhaseCommitServices extends tpcImplBase {
         String clientID = request.getClientID();
         Integer timestamp = request.getTimestamp();
 
-        /* temporary operations in notifying service */
+        /* operations in notifying service */
+        String writeVariable;
 
+        int numOfWriteVariable = request.getWriteToCount();
+        for (int index=0; index<numOfWriteVariable; index++) {
+            writeVariable = request.getWriteTo(numOfWriteVariable);
+            this.variableMap.replace(writeVariable, false);
+        }
 
-        /* generate and send the response */
+        /* generating and sending the response */
         this.updateTimestamp(timestamp);
         Empty empty = this.generateEmpty();
         responseObserver.onNext(empty);
